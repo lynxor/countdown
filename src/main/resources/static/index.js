9ee76da2b6f890dce92d890321b7b85a3693a371
Nico Kruger <nico.kruger@gmail.com>
2011-12-01 21:20:50 +0200

    Countdowns now get placed in asc order by eventDate.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -7,7 +7,7 @@ $(function () {
             $("#countdownlist").html(""); // clear
             // iterate through countdowns
            _(o.countdowns).each(function (countdownInfo) {
-               placeCountdown(countdownInfo);
+               model.getCountdown(countdownInfo);
             });           
         },
         error: function (o) {
@@ -28,21 +28,55 @@ var ledColors = {
     outline: "rgba(0, 0, 0, 0.0)"
 }
 
-var placeCountdown = function (countdownInfo) {
+var findByDate = function (countdownInfo) {
+    for (var i  = 0; i < this.countdowns; i++) {
+        if (countdownInfo[i].eventDate > countdownInfo.eventDate) {
+            return countdownInfo[i].url;
+        }
+    }
+    return undefined;
+}
+
+var model = {
+    
+    countdowns: [],
+}
+
+model.find = _.bind(findByDate, model);
+
+model.putCountdown = function (countdownInfo, c) {
+    
+    var where = this.find(c);
+    var outside;
+    var where = this.find(c);
+    
+    if (where === undefined) {
+        outside = $('<li></li>').appendTo("#countdownlist");
+    } else {
+        outside = $("<li></li>").insertAfter($("#" + where));
+    }
+    $(outside).append("<h5>" + countdownInfo.label + "</h5>");
+    $(outside).append("<div id=\"" + countdownInfo.url + "\"></div>");
+    
+    countdown($("#" + countdownInfo.url), c.eventDate, 24, 32, ledColors);
+    
+    $("#countdownlist").listview("refresh");
+}
+
+model.getCountdown = function (countdownInfo) {
     $.ajax({
         url: "/countdown/" + countdownInfo.url,
         dataType: "json",
-        success: function (c) {
-            var outside = $('<li></li>').appendTo("#countdownlist");
-            $(outside).append("<h5>" + countdownInfo.label + "</h5>");
-            $(outside).append("<div id=\"" + countdownInfo.url + "\"></div>");
-            
-            countdown($("#" + countdownInfo.url), c.eventDate, 24, 32, ledColors);
-            
-            $("#countdownlist").listview("refresh");
-        },
+        success: _.bind(this.putCountdown, model, countdownInfo),
         error: function (o) {
-            $("#" + countdownInfo.url).html("Error retrieving countdown timer");
+            // TODO: handle the error
         }
     });
 }
+
+model.clear = function () {
+    this.countdowns = [];
+    $("#countdownlist").html(""); // clear
+ }
+ 
+ 
